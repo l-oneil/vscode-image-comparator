6e1d05393d522e4dd28493d1a58b968365366050
l-oneil <60259140+l-oneil@users.noreply.github.com>
2022-10-09 18:12:21 +0100

    `AddImage` works via `cmd+shift+p` now too;

--- src/extension.ts
+++ src/extension.ts
@@ -10,7 +10,25 @@ export function activate(context: vscode.ExtensionContext) {
 	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand('image-comparator.addImage', function (uri: vscode.Uri) {
-		// Add Image to Comparator
-		ImageViewer.registerImage(uri.fsPath);
+		if (uri != undefined) {
+			// Add Image from context menu to Comparator
+			ImageViewer.registerImage(uri.fsPath);
+		}
+		else {
+			// Search Dialog to Add x1 Image to Comparator
+			const options: vscode.OpenDialogOptions = {
+				canSelectMany: false,
+				openLabel: 'Open',
+				filters: {
+					'Images': ['png', 'jpg', 'jpeg'],
+				}
+			};
+			vscode.window.showOpenDialog(options).then(fileUri => {
+				if (fileUri && fileUri[0]) {
+					// Add Image to Comparator
+					ImageViewer.registerImage(fileUri[0].fsPath);
+				}
+			});
+		}
 	}));
 }
