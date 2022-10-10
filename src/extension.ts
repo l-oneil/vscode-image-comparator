c7751354af0d649a8a9b19c550e5eb2cd432c20a
liaone01 <liam.oneil@arm.com>
2022-10-10 16:50:54 +0100

    `Add Image` now opens a Comparator Window if one doesn't exist, or otherwise switches to active one;

--- src/extension.ts
+++ src/extension.ts
@@ -10,6 +10,9 @@ export function activate(context: vscode.ExtensionContext) {
 	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand('image-comparator.addImage', function (uri: vscode.Uri) {
+		// Open Comparator Window, in case it's not already open
+		ImageViewer.createOrShow(context.extensionUri);
+
 		if (uri != undefined) {
 			// Add Image from context menu to Comparator
 			ImageViewer.registerImage(uri.fsPath);
@@ -23,6 +26,7 @@ export function activate(context: vscode.ExtensionContext) {
 					'Images': ['png', 'jpg', 'jpeg'],
 				}
 			};
+
 			vscode.window.showOpenDialog(options).then(fileUri => {
 				if (fileUri && fileUri[0]) {
 					// Loop through and add each Image to Comparator
