32c3bdcd18fc7616630ee825fc8892f41c666244
l-oneil <60259140+l-oneil@users.noreply.github.com>
2022-12-31 14:37:38 +0000

    Refine SO link; Add typehint;

--- src/extension.ts
+++ src/extension.ts
@@ -2,22 +2,22 @@ import * as vscode from 'vscode';
 import { ImageViewer } from './image_viewer.js';
 
 
-// https://stackoverflow.com/questions/70303516/how-to-get-the-multi-selected-files-from-the-file-explorer-in-an-extension-comma
 
 export function activate(context: vscode.ExtensionContext) {
-
+	
 	context.subscriptions.push(vscode.commands.registerCommand('image-comparator.openViewer', function () {
 		// Open Comparator Window
 		ImageViewer.createOrShow(context.extensionUri);
 	}));
-
+	
 	context.subscriptions.push(vscode.commands.registerCommand('image-comparator.addImage', async (...inputArgs) => {
+		// https://stackoverflow.com/a/70307717/7861160
 		// Open Comparator Window, in case it's not already open
 		ImageViewer.createOrShow(context.extensionUri);
 		
 		// Loop over all images in context menu list
 		if (inputArgs[0] != undefined) {
-			inputArgs[1].forEach((uri) => {
+			inputArgs[1].forEach((uri: vscode.Uri) => {
 				if (uri instanceof vscode.Uri) {
 					console.log(uri.fsPath);
 					ImageViewer.registerImage(uri.fsPath);
